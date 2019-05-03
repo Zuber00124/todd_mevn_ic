2e6901186b9922e6302e2e666647aa398316c9e2
Zuber00124 <zuber00124@gmail.com>
2019-05-03 22:13:59 +0530

    part 2

---
+++ insta-client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #f7f7f7;
+  font-family: "Roboto", Helvetica, sans-serif;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  min-height: 100vh;
+}
+</style>
---
+++ insta-client/src/router.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import Register from './views/Register.vue'
+
+Vue.use(Router)
+
+let router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: {
+        requiresAuth: false
+      }
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem('jwt') == null) {
+      next({
+        path: '/login',
+        params: { nextURL: to.fullPath }
+      })
+    } else {
+      // console.log('Logged in')
+      next()
+    }
+  } else {
+    // console.log("Doesn't require Auth")
+    next()
+  }
+})
+
+export default router
